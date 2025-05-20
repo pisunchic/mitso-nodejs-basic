e5fe7e380b95000deccc35fbc229c672a3fd6b30
pisunchic <mskdkeke@gmail.com>
2025-05-20 21:08:25 +0300

    реализация read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+    
+    // Создаем Readable Stream для чтения файла
+    const readStream = createReadStream(filePath, { encoding: 'utf8' });
+    
+    // Напрямую подключаем readStream к process.stdout
+    readStream.pipe(process.stdout);
+    
+    // Обработка ошибок
+    readStream.on('error', (error) => {
+        console.error('Error reading file:', error.message);
+        process.exit(1);
+    });
 };
 
 await read();
