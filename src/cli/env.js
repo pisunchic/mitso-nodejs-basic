a261a15cd3b9f92dd6f57b8c20cd5f9d893eaddc
pisunchic <mskdkeke@gmail.com>
2025-05-20 20:49:05 +0300

    реализация env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const MITSO_VARS = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('MITSO_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    if (MITSO_VARS) {
+        console.log(MITSO_VARS);
+    }
 };
 
 parseEnv();
