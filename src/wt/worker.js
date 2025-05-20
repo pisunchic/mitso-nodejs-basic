b4b7cde5fb7e2c89b3cdd3b53341403d27961602
pisunchic <mskdkeke@gmail.com>
2025-05-20 21:27:30 +0300

    реализация worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,21 @@
+import { parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    // Получаем данные из основного потока
+    parentPort.on('message', (n) => {
+        try {
+            // Вычисляем n-ое число Фибоначчи
+            const result = nthFibonacci(n);
+            // Отправляем результат в основной поток
+            parentPort.postMessage({ status: 'resolved', data: result });
+        } catch (error) {
+            // В случае ошибки отправляем статус error
+            parentPort.postMessage({ status: 'error', data: null });
+        }
+    });
 };
 
 sendResult();
