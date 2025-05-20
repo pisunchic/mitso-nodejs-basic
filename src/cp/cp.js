ef0307ca8d17a8d9b992ccb4b75e6f2ae1260416
pisunchic <mskdkeke@gmail.com>
2025-05-20 21:35:53 +0300

    реализация cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,31 @@
+import { spawn } from 'child_process';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    // Создаем дочерний процесс
+    const childProcess = spawn('node', [join(__dirname, 'script.js'), ...args], {
+        stdio: ['inherit', 'inherit', 'inherit'] // Наследуем stdin, stdout и stderr
+    });
+    
+    // Обработка ошибок дочернего процесса
+    childProcess.on('error', (error) => {
+        console.error('Child process error:', error);
+        process.exit(1);
+    });
+    
+    // Обработка завершения дочернего процесса
+    childProcess.on('exit', (code) => {
+        if (code !== 0) {
+            console.error(`Child process exited with code ${code}`);
+            process.exit(code);
+        }
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// Пример использования
+spawnChildProcess(['arg1', 'arg2']);
